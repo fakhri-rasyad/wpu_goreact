e3a9d6c359f5b3dbf5f8e8fa796ccfb616e40159
fakhri-rasyad <kaizerd23@gmail.com>
2026-03-01 11:20:55 +0800

    feat: added api response for user

--- go.mod
+++ go.mod
@@ -8,7 +8,11 @@ require (
 )
 
 require (
+	github.com/andybalholm/brotli v1.2.0 // indirect
+	github.com/gofiber/fiber v1.14.6 // indirect
+	github.com/gofiber/utils v1.2.0 // indirect
 	github.com/golang-jwt/jwt/v5 v5.3.1 // indirect
+	github.com/gorilla/schema v1.4.1 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/pgx/v5 v5.6.0 // indirect
@@ -16,8 +20,15 @@ require (
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/klauspost/compress v1.18.4 // indirect
+	github.com/mattn/go-colorable v0.1.14 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.69.0 // indirect
+	github.com/valyala/tcplisten v1.0.0 // indirect
 	golang.org/x/crypto v0.48.0 // indirect
 	golang.org/x/sync v0.19.0 // indirect
+	golang.org/x/sys v0.41.0 // indirect
 	golang.org/x/text v0.34.0 // indirect
 	gorm.io/driver/postgres v1.6.0 // indirect
 )
---
+++ utils/response.go
@@ -0,0 +1,49 @@
+package utils
+
+import "github.com/gofiber/fiber"
+
+type Response struct {
+	Status       string      `json:"status"`
+	ResponseCode int         `json:"response_code"`
+	Message      string      `json:"message,omitempty"`
+	Data         interface{} `json:"data,omitempty"`
+	Error        interface{} `json:"error,omitempty"`
+}
+
+func Success(ctx *fiber.Ctx, message string, data interface{}) error {
+	return ctx.Status(fiber.StatusOK).JSON(Response{
+		Status: "Success",
+		ResponseCode: fiber.StatusOK,
+		Message: message,
+		Data: data,
+	})
+}
+
+func Created(ctx *fiber.Ctx, message string, data interface{}) error {
+	return ctx.Status(fiber.StatusCreated).JSON(Response{
+		Status: "Created",
+		ResponseCode: fiber.StatusCreated,
+		Message: message,
+		Data: data,
+	})
+}
+
+func BadRequest(ctx *fiber.Ctx, message string, data interface{}, err string) error {
+	return ctx.Status(fiber.StatusBadRequest).JSON(Response{
+		Status: "Error bad request",
+		ResponseCode: fiber.StatusBadRequest,
+		Message: message,
+		Data: data,
+		Error: err,
+	})
+}
+
+func NotFound(ctx *fiber.Ctx, message string, data interface{}, err string) NotFound {
+	return ctx.Status(fiber.StatusNotFound).JSON(Response{
+		Status: "Error not found",
+		ResponseCode: fiber.StatusNotFound,
+		Message: message,
+		Data: data,
+		Error: err,
+	})
+}
