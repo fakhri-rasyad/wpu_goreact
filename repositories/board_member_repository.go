2a81d9f9a5f803d8edada44b22659f20b6b30610
fakhri-rasyad <kaizerd23@gmail.com>
2026-03-05 15:18:31 +0800

    Fix minor issue on board member repositoryt

--- repositories/board_member_repository.go
+++ repositories/board_member_repository.go
@@ -20,7 +20,7 @@ func NewBoardMemberRepository() BoardMemberRepository {
 func (repo *BoardMemberRepostioryImpl) GetMembers(boardPublicId string) ([]models.User, error) {
 	var users []models.User 
 	err := config.DB.Joins("JOIN board_members ON board_members.user_internal_id = users.internal_id").
-	Joins("JOIN board_members ON board_members.board_internal_id = boards.internal_id").
+	Joins("JOIN boards ON boards.internal_id = board_members.board_internal_id").
 	Where("boards.public_id = ?", boardPublicId).
 	Find(&users).Error
 
